1c0427fdae8e5cdd0faf8d65fed754e61cf7192e
Thiago Ueda <thiagokoiti@gmail.com>
2024-09-09 09:39:43 -0300

    Adiciona animação dos numeros utilizando o Jquery

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
-const numbers = document.querySelectorAll("[data-number]");
+$(function () {
 
-if (numbers.length > 0) {
-  const startNumberAnimation = (element) => {
-    const number = +element.innerText;
-    const numberDivision = number / 30;
-    const animationRuntimeMS = 50;
-    let dynamicNumber = 0;
 
-    element.innerText = dynamicNumber;
-
-    const animateNumbers = setInterval(() => {
-      if (dynamicNumber < number) {
-        dynamicNumber += numberDivision;
-        element.innerText = Math.floor(dynamicNumber);
-      } else {
-        element.innerText = number;
-        clearInterval(animateNumbers);
-      }
-    }, Math.random() * animationRuntimeMS);
-  };
-
-  numbers.forEach((number) => {
-    startNumberAnimation(number);
+  //HEADER
+  $(window).scroll(function () {
+        if($(this).scrollTop() > 200)
+        {
+            if (!$('.main_header').hasClass('fixed'))
+            {
+                $('.main_header').stop().addClass('fixed').css('top', '-100px').animate(
+                    {
+                        'top': '0px'
+                    }, 500);
+            }
+        }
+        else
+        {
+            $('.main_header').removeClass('fixed');
+        }
   });
-}
+
+
+});
+
+
+$('.number').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  });
+});
